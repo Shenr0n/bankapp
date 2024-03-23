637154f6548afc0534b71544e8b014044668ea41
Shenr0n <xenonsid007@gmail.com>
2024-03-22 20:49:48 -0400

    removed route

--- api/server.go
+++ api/server.go
@@ -62,7 +62,7 @@ func (server *Server) setupRouter() {
 	authRoutes.POST("/accounts", server.createAccount)
 	authRoutes.GET("/accounts/:id", server.getAccount)
 	authRoutes.GET("/accounts", server.listAccounts)
-	authRoutes.PUT("/accounts/:id/update/name", server.updateAccountName)
+	//authRoutes.PUT("/accounts/:id/update/name", server.updateAccountName)
 	authRoutes.DELETE("/accounts/:id/delete", server.deleteAccount)
 
 	//Entry
